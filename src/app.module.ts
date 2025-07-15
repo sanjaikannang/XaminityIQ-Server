fd663b2de7e07c7d55092c99027df569b171707e
Sanjai Kannan G <136103561+sanjaikannang@users.noreply.github.com>
2025-07-15 20:32:41 +0530

    Integrate MongoDB with Mongoose in NestJS app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.getMongoDbUri(),
+        connectionFactory: (connection) => {
+          connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
+          });
+          connection.once('open', () => {
+            console.log('MongoDB connection opened');
+          });
+          connection.on('error', (err) => {
+            console.error('MongoDB connection error:', err);
+          });
+          return connection;
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
+  exports: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8004);
 }
 bootstrap();
