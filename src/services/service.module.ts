200f8915a4a396419833fa17cfa83fdc5ae68382
Sanjai Kannan G <136103561+sanjaikannang@users.noreply.github.com>
2025-07-19 09:54:32 +0530

    Updated Service module for JwtModule and RedisModule

--- src/services/service.module.ts
+++ src/services/service.module.ts
@@ -1,4 +1,6 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { RedisModule } from "@nestjs-modules/ioredis";
 
 // Services
 import { AuthService } from "./auth-service/auth.service";
@@ -8,18 +10,32 @@ import { SessionService } from "./auth-service/session.service";
 import { AdminService } from "./user-service/admin/admin.service";
 import { FacultyService } from "./user-service/faculty/faculty.service";
 import { StudentService } from "./user-service/student/student.service";
+import { ConfigService } from "src/config/config.service";
 
 // Modules
 import { ConfigModule } from "src/config/config.module";
 import { RepositoryModule } from "src/repositories/repository.module";
-import { JwtModule } from "@nestjs/jwt";
-
 
 @Module({
     imports: [
         RepositoryModule,
         ConfigModule,
-        JwtModule.register({})
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.getJWTSecretKey(),
+                signOptions: { expiresIn: configService.getJWTExpiresIn() },
+            }),
+            inject: [ConfigService],
+        }),
+        RedisModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                type: 'single',
+                url: `redis://:${configService.getRedisPassword()}@${configService.getRedisHost()}:${configService.getRedisPort()}`,
+            }),
+            inject: [ConfigService],
+        }),
     ],
     controllers: [],
     providers: [
